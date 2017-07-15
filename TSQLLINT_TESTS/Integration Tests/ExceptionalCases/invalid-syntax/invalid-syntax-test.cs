4e3b1a98f129b851f85afbbc23b19245e737e7b2
Nathan Boyd <nathan@engineernathan.org>
2017-07-14 22:37:08 -0400

    added test for invalid path parsing

--- TSQLLINT_TESTS/Integration Tests/ExceptionalCases/invalid-syntax/invalid-syntax-test.cs
+++ TSQLLINT_TESTS/Integration Tests/ExceptionalCases/invalid-syntax/invalid-syntax-test.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using NUnit.Framework;
 using TSQLLINT_LIB.Config;
 using TSQLLINT_LIB.Parser;
 using TSQLLINT_LIB.Parser.Interfaces;
+using TSQLLINT_LIB.Rules.RuleViolations;
 
 namespace TSQLLINT_LIB_TESTS.Integration_Tests.ExceptionalCases
 {
@@ -16,9 +18,21 @@ namespace TSQLLINT_LIB_TESTS.Integration_Tests.ExceptionalCases
 
             ILintConfigReader configReader = new LintConfigReader(Path.Combine(lintTarget, ".tsqllintrc"));
             IRuleVisitor ruleVisitor = new SqlRuleVisitor(configReader);
+            IResultReporter testReporter = new IntegrationExceptionalCaseTestReporter();
             var fileProcessor = new SqlFileProcessor(ruleVisitor);
 
-            Assert.Throws<Exception>(() => fileProcessor.ProcessPath(lintTarget));
+            var lintFile = Path.Combine(TestContext.CurrentContext.TestDirectory, "..\\..\\Integration Tests\\ExceptionalCases\\invalid-syntax\\invalid-syntax.sql");
+            fileProcessor.ProcessPath(lintFile);
+            testReporter.ReportResults(ruleVisitor.Violations);
+        }
+    }
+
+    public class IntegrationExceptionalCaseTestReporter: IResultReporter
+    {
+        public void ReportResults(List<RuleViolation> violations)
+        {
+            Assert.AreEqual(1, violations.Count);
+            Assert.AreEqual("TSQL not syntactically correct", violations[0].Text);
         }
     }
 }
