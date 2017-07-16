c5e1144a066a4c74b7f1ca173cf579dae3eb465f
Nathan Boyd <nathan@engineernathan.org>
2017-07-16 02:14:15 -0400

    additional coverage for select star

--- TSQLLINT_TESTS/Unit Tests/Rules/select-star/select-star-no-error.sql
+++ TSQLLINT_TESTS/Unit Tests/Rules/select-star/select-star-no-error.sql
@@ -1,6 +1,14 @@
-﻿--SELECT FOO FROM BAR;
+﻿-- simple select
+SELECT FOO FROM BAR;
 
+-- select star within exists predicate
 IF EXISTS (SELECT * from sys.triggers WHERE name='Foo' and type='TR')
+BEGIN
+	PRINT 'FOO'
+END
+
+-- select within exists predicate 
+IF EXISTS (SELECT 1)
 BEGIN
 	PRINT 'FOO'
 END
