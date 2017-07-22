08f9224f3d3000d5a38ffb2a14dd67bbfc7c48f3
Nathan Boyd <nathan@engineernathan.org>
2017-07-22 13:29:05 -0400

    Release 0.1.30

--- TSQLLINT_CONSOLE/Properties/AssemblyInfo.cs
+++ TSQLLINT_CONSOLE/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("5292f15d-1f0a-47ce-928f-9fdc1929792b")]
 
-[assembly: AssemblyVersion("0.1.29.0")]
-[assembly: AssemblyFileVersion("0.1.29.0")]
+[assembly: AssemblyVersion("0.1.30.0")]
+[assembly: AssemblyFileVersion("0.1.30.0")]
