3a99094fca4d2a501bc82fff0b918f9ef9345dcb
Nathan Boyd <nathan@engineernathan.org>
2017-07-24 13:15:16 -0400

    moved util, added json validation

---
+++ TSQLLINT_LIB/Utility/Utility.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace TSQLLINT_LIB
+{
+    public static class Utility
+    {
+        public static TextReader CreateTextReaderFromString(string str)
+        {
+            var bytes = Encoding.UTF8.GetBytes(str);
+            var memoryStream = new MemoryStream(bytes);
+
+            return new StreamReader(memoryStream);
+        }
+
+        public static string GetFileContents(string filePath)
+        {
+            return File.ReadAllText(filePath);
+        }
+
+        public static bool IsValidJson(string strInput)
+        {
+            strInput = strInput.Trim();
+            if ((strInput.StartsWith("{") && strInput.EndsWith("}")) ||
+                (strInput.StartsWith("[") && strInput.EndsWith("]")))
+            {
+                try
+                {
+                    JToken.Parse(strInput);
+                    return true;
+                }
+                catch (JsonReaderException jex)
+                {
+                    //Exception in parsing json
+                    Console.WriteLine(jex.Message);
+                    return false;
+                }
+                catch (Exception ex) //some other exception
+                {
+                    Console.WriteLine(ex.ToString());
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
