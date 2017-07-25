6f919db4a0e6a6f3273d9330aa66ae3c6300c255
Nathan Boyd <nathan@engineernathan.org>
2017-07-24 21:51:35 -0400

    updated versioning script

--- scripts/version-assembly.cmd
+++ scripts/version-assembly.cmd
@@ -1,4 +1,1 @@
-npm version arg0 > temp.txt
-set /p VER=<temp.txt
-set VER=%VER:~1%
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '.\scripts\ps\set-assembly-version.ps1' %VER"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '.\scripts\ps\set-assembly-version.ps1' %1" 
