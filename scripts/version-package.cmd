218bb20019528fc6ab6cfd99211e4bfa68c3e20b
Nathan Boyd <nathan@engineernathan.org>
2017-08-16 06:41:37 -0400

    Fix assembly versioning script
      - Remove 'v' from assembly version string

--- scripts/version-package.cmd
+++ scripts/version-package.cmd
@@ -1,3 +1,4 @@
 for /f %%i in ('npm version %1 --no-git-tag-version') do set VERSION=%%i
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '.\scripts\ps\set-assembly-version.ps1' %VERSION%"
+set ASSEMBLY_VERSION=%VERSION:~1%
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '.\scripts\ps\set-assembly-version.ps1' %ASSEMBLY_VERSION%"
 git commit -a -m "Release %VERSION%" && git tag -a -m "Release %VERSION%" %VERSION%
