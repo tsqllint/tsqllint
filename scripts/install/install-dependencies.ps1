56653ac6d741c611850a5f8b64238ba9e1bba6d3
Douglas Christopher Wilson <doug@somethingdoug.com>
2017-08-23 17:29:20 -0400

    Use npm config prefix for postinstall script

--- scripts/install/install-dependencies.ps1
+++ scripts/install/install-dependencies.ps1
@@ -23,7 +23,7 @@ Catch {
 
 	# copy dll from unzipped package into tsqllint bin directory
 	$dll = $unzip_path + "/lib/" + $package_name + ".dll"
-	$destination = $env:APPDATA + "/npm/node_modules/tsqllint/TSQLLINT_CONSOLE/bin/Release/"
+	$destination = $env:npm_config_prefix + "/node_modules/tsqllint/TSQLLINT_CONSOLE/bin/Release/"
 	Copy-Item $dll -Destination $destination
 
 	# cleanup
