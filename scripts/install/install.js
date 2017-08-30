d9b0dde55a1c1ba240a61f7e4340069232ede818
Douglas Christopher Wilson <doug@somethingdoug.com>
2017-08-30 15:14:08 -0400

    Fix references to APPDATA

--- scripts/install/install.js
+++ scripts/install/install.js
@@ -2,7 +2,7 @@ var fs = require('fs')
 const execSync = require('child_process').execSync;
 
 // calls powershell script to install missing dependencies
-execSync(`call ${process.env.APPDATA}/npm/node_modules/tsqllint/scripts/install/install-dependencies.cmd`);
+execSync(`call ${__dirname}/install-dependencies.cmd`);
 
 // copy patched version of tsqllint script
-fs.createReadStream('./scripts/install/tsqllint').pipe(fs.createWriteStream(`${process.env.APPDATA}/npm/tsqllint`))
+fs.createReadStream(`${__dirname}/tsqllint`).pipe(fs.createWriteStream(`${process.env.npm_config_prefix}/tsqllint`))
