c638d49eecea6765d3ba28dec135a592948ae75a
Nathan Boyd <nboyd@salesforce.com>
2017-08-30 16:13:08 -0400

    Release v1.1.4

--- TSQLLINT_CONSOLE/Properties/AssemblyInfo.cs
+++ TSQLLINT_CONSOLE/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("5292f15d-1f0a-47ce-928f-9fdc1929792b")]
 
-[assembly: AssemblyVersion("1.1.3")]
-[assembly: AssemblyFileVersion("1.1.3")]
+[assembly: AssemblyVersion("1.1.4")]
+[assembly: AssemblyFileVersion("1.1.4")]
