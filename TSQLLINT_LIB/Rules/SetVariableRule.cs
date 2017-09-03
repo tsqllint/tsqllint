94086e0b3a8288eda221feb222bbc43a9b50b463
Nathan Boyd <nboyd@salesforce.com>
2017-09-02 20:39:50 -0400

    Add set variable rule
      - Create new set variable rule tests
      - Create new rule
      - Update config generator
      - Update rule parser

---
+++ TSQLLINT_LIB/Rules/SetVariableRule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.SqlServer.TransactSql.ScriptDom;
+using TSQLLINT_LIB.Rules.Interface;
+
+namespace TSQLLINT_LIB.Rules
+{
+    public class SetVariableRule : TSqlFragmentVisitor, ISqlRule
+    {
+        public string RULE_NAME { get { return "set-variable"; } }
+        public string RULE_TEXT { get { return "Expected variable to be set using SELECT statement"; } }
+        public Action<string, string, int, int> ErrorCallback;
+
+        public SetVariableRule(Action<string, string, int, int> errorCallback)
+        {
+            ErrorCallback = errorCallback;
+        }
+
+        public override void Visit(SetVariableStatement node)
+        {
+            ErrorCallback(RULE_NAME, RULE_TEXT, node.StartLine, node.StartColumn);
+        }
+    }
+}
---
+++ TSQLLINT_TESTS/UnitTests/Rules/set-variable/set-variable-one-error-mixed-state.sql
@@ -0,0 +1,6 @@
+﻿DECLARE @var1 varchar(30)
+
+SELECT @var1 = 'foo'
+
+-- should error
+SET @var1 = 'bar'
