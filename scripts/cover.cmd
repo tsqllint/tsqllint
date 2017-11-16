fb28e445a1206260a98f2d25d78b647570ef2c56
Nathan Boyd <nboyd@salesforce.com>
2017-11-15 22:53:58 -0500

    Update coverage script to work with dotnet core

--- scripts/cover.cmd
+++ scripts/cover.cmd
@@ -1,4 +1,26 @@
+nuget install -Verbosity quiet -OutputDirectory packages -Version 4.6.519 OpenCover
+nuget install -Verbosity quiet -OutputDirectory packages -Version 3.0.2 ReportGenerator
+nuget install -Verbosity quiet -OutputDirectory packages -Version 1.0.1 Codecov
+
+SET OPENCOVER=.\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe
+SET REPORTGENERATOR=.\packages\ReportGenerator.3.0.2\tools\ReportGenerator.exe
+SET CODECOV=.\packages\Codecov.1.0.1\tools\codecov.exe
+
+RMDIR /S /Q coverage
 mkdir coverage
-.\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -excludebyattribute:*.ExcludeFromCodeCoverage* -register:user -output:.\coverage\coverage_results.xml "-target:.\packages\NUnit.ConsoleRunner.3.6.1\tools\nunit3-console.exe" "-targetargs:.\TSQLLint.Tests\bin\Release\TSQLLint.Tests.dll"
-.\packages\ReportGenerator.2.5.8\tools\ReportGenerator.exe "-reports:.\coverage\coverage_results.xml " "-targetdir:.\coverage\report\"
-.\packages\Codecov.1.0.1\tools\codecov.exe -f ".\coverage\coverage_results.xml " -t %codecov_token%
+
+%OPENCOVER% ^
+    -excludebyattribute:*.ExcludeFromCodeCoverage* ^
+    -register:user ^
+    -output:".\coverage\coverage_results.xml" ^
+    -target:"C:\Program Files\dotnet\dotnet.exe" ^
+    -targetargs:"test .\TSQLLint.Tests\TSQLLint.Tests.csproj" ^
+    -oldStyle
+
+%REPORTGENERATOR% ^
+    -reports:.\coverage\coverage_results.xml ^
+    -targetdir:.\coverage\report\
+
+%CODECOV% ^
+    -f ".\coverage\coverage_results.xml" ^
+    -t %codecov_token%
