df5726b90d1c49c07cd57d27c6773689a4d667a4
Nathan Boyd <nathan@engineernathan.org>
2017-12-07 20:20:42 -0500

    Add log4net
      - Create trace listener
      - Create trace listener tests

--- TSQLLint.Lib/Utility/Log4netTraceListener.cs
+++ TSQLLint.Lib/Utility/Log4netTraceListener.cs
@@ -1,33 +1,33 @@
-//using System.Diagnostics;
-//using log4net;
+using System.Diagnostics;
+using log4net;
 
-//namespace TSQLLint.Lib.Utility
-//{
-//    public class Log4netTraceListener : TraceListener
-//    {
-//        private readonly ILog _log;
-//        private readonly bool LoggingEnabled;
+namespace TSQLLint.Lib.Utility
+{
+   public class Log4netTraceListener : TraceListener
+   {
+       private readonly ILog _log;
+       private readonly bool LoggingEnabled;
 
-//        public Log4netTraceListener(ILog logger, bool loggingEnabled)
-//        {
-//            _log = logger;
-//            LoggingEnabled = loggingEnabled;
-//        }
+       public Log4netTraceListener(ILog logger, bool loggingEnabled)
+       {
+           _log = logger;
+           LoggingEnabled = loggingEnabled;
+       }
 
-//        public override void Write(string message)
-//        {
-//            if (LoggingEnabled)
-//            {
-//                _log.Debug(message);
-//            }
-//        }
+       public override void Write(string message)
+       {
+           if (LoggingEnabled)
+           {
+               _log.Debug(message);
+           }
+       }
 
-//        public override void WriteLine(string message)
-//        {
-//            if (LoggingEnabled)
-//            {
-//                _log.Debug(message);
-//            }
-//        }
-//    }
-//}
+       public override void WriteLine(string message)
+       {
+           if (LoggingEnabled)
+           {
+               _log.Debug(message);
+           }
+       }
+   }
+}
--- TSQLLint.Tests/UnitTests/Utility/LoggingTests.cs
+++ TSQLLint.Tests/UnitTests/Utility/LoggingTests.cs
@@ -1,46 +1,46 @@
-//using System.Diagnostics;
-//using log4net;
-//using NSubstitute;
-//using NUnit.Framework;
-//using TSQLLint.Lib.Utility;
+using NSubstitute;
+using NUnit.Framework;
+using System.Diagnostics;
+using TSQLLint.Lib.Utility;
+using log4net;
 
-//namespace TSQLLint.Tests.UnitTests.Utility
-//{
-//    [TestFixture]
-//    public class LoggingTests
-//    {
-//        [Test]
-//        public void TraceListener_LoggingEnabled_ShouldLog()
-//        {
-//            // arrange
-//            var mockLogger = Substitute.For<ILog>();
-//            var loggingEnabled = true;
-//            var traceListener = new Log4netTraceListener(mockLogger, loggingEnabled);
+namespace TSQLLint.Tests.UnitTests.Utility
+{
+   [TestFixture]
+   public class LoggingTests
+   {
+       [Test]
+       public void TraceListener_LoggingEnabled_ShouldLog()
+       {
+           // arrange
+           var mockLogger = Substitute.For<ILog>();
+           var loggingEnabled = true;
+           var traceListener = new Log4netTraceListener(mockLogger, loggingEnabled);
 
-//            // act
-//            traceListener.Write("Write");
-//            traceListener.WriteLine("Writeline");
+           // act
+           traceListener.Write("Write");
+           traceListener.WriteLine("Writeline");
 
-//            // assert
-//            mockLogger.Received().Debug("Write");
-//            mockLogger.Received().Debug("Writeline");
-//        }
+           // assert
+           mockLogger.Received().Debug("Write");
+           mockLogger.Received().Debug("Writeline");
+       }
 
-//        [Test]
-//        public void TraceListener_LoggingDisabled_ShouldNotLog()
-//        {
-//            // arrange
-//            var mockLogger = Substitute.For<ILog>();
-//            var loggingEnabled = false;
-//            var traceListener = new Log4netTraceListener(mockLogger, loggingEnabled);
+       [Test]
+       public void TraceListener_LoggingDisabled_ShouldNotLog()
+       {
+           // arrange
+           var mockLogger = Substitute.For<ILog>();
+           var loggingEnabled = false;
+           var traceListener = new Log4netTraceListener(mockLogger, loggingEnabled);
 
-//            // act
-//            traceListener.Write("Write");
-//            traceListener.WriteLine("Writeline");
+           // act
+           traceListener.Write("Write");
+           traceListener.WriteLine("Writeline");
 
-//            // assert
-//            mockLogger.DidNotReceive().Debug("Write");
-//            mockLogger.DidNotReceive().Debug("Writeline");
-//        }
-//    }
-//}
+           // assert
+           mockLogger.DidNotReceive().Debug("Write");
+           mockLogger.DidNotReceive().Debug("Writeline");
+       }
+   }
+}
