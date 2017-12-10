83dae13cd4c1d36c8d702eae854e586a602e6dcd
Nathan Boyd <nathan@engineernathan.org>
2017-12-10 15:22:37 -0500

    Remove unused scripts

--- scripts/build.cmd
+++
@@ -1,5 +0,0 @@
-REM install nuget packages
-nuget restore "TSQLLint.sln"
-
-REM build release
-MsBuild.exe "TSQLLint.sln" /t:Clean;Build /p:Configuration=Release /p:TargetFramework=v4.5.2
--- scripts/install/install.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs')
-const execSync = require('child_process').execSync;
-
-// calls powershell script to install missing dependencies
-execSync(`call ${__dirname}/install-dependencies.cmd`);
-
-// copy patched version of tsqllint script
-fs.createReadStream(`${__dirname}/tsqllint`).pipe(fs.createWriteStream(`${process.env.npm_config_prefix}/tsqllint`))
