ed84c32116a82dbf9eeabf7130ff95a13880353e
Nathan Boyd <nathan-boyd@users.noreply.github.com>
2017-12-10 19:21:29 -0800

    Update install script to rely upon self contained deployment (#104)
    * Remove unused scripts
    * Add package and install scripts
    * Update install script to:
      - Download runtime archives from github
      - Unzip Archives
    * Update path in bin file
    * Remove lib path from package.json
    * Update install script to pull os runtime
    * Update install script to work with windows x86
    * Update tsqllint bin file to call windows exe by runtime

---
+++ scripts/packageRuntimes/packageRuntimes.sh
@@ -0,0 +1,3 @@
+cd lib && tar -zcvf osx-x64.tar.gz osx-x64 && cd ..
+cd lib && tar -zcvf win-x86.tar.gz win-x86 && cd ..
+cd lib && tar -zcvf win-x64.tar.gz win-x64 && cd ..
--- tsqllint.js
+++ tsqllint.js
@@ -5,11 +5,17 @@ const os = require('os');
 var args = process.argv.slice(2);
 
 if (os.type() === 'Linux' || os.type() === 'Darwin') {
-  args.unshift(`${__dirname}/lib/netcoreapp2.0/TSQLLint.Console.dll`)
+  args.unshift(`${__dirname}/osx-x64/TSQLLint.Console.dll`)
   var child = spawn('dotnet', args);
 }
 else if (os.type() === 'Windows_NT') {
-  var child = spawn(`${__dirname}/lib/net47/TSQLLint.Console.exe`, args);
+  if (os.type() === 'Windows_NT') {
+    if (process.arch == 'x32') {
+      var child = spawn(`${__dirname}/win-x86/TSQLLint.Console.exe`, args);
+    } else if (process.arch == 'x64') {
+      var child = spawn(`${__dirname}/win-x64/TSQLLint.Console.exe`, args);
+    }
+  } 
 }
 else {
   throw new Error(`Invalid Platform: ${os.type()}`)
