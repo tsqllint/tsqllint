6c84b129daa8472737853e75271c90297899f7fc
Nathan Boyd <nathan@engineernathan.org>
2017-12-10 20:37:35 -0500

    Update path in bin file

--- tsqllint.js
+++ tsqllint.js
@@ -5,11 +5,11 @@ const os = require('os');
 var args = process.argv.slice(2);
 
 if (os.type() === 'Linux' || os.type() === 'Darwin') {
-  args.unshift(`${__dirname}/lib/netcoreapp2.0/TSQLLint.Console.dll`)
+  args.unshift(`${__dirname}/lib/TSQLLint.Console.dll`)
   var child = spawn('dotnet', args);
 }
 else if (os.type() === 'Windows_NT') {
-  var child = spawn(`${__dirname}/lib/net47/TSQLLint.Console.exe`, args);
+  var child = spawn(`${__dirname}/lib/TSQLLint.Console.exe`, args);
 }
 else {
   throw new Error(`Invalid Platform: ${os.type()}`)
