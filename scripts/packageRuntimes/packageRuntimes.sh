2c15c23bc9d0d590253b1a107aefac930c97f7e6
Nathan Boyd <nathan@engineernathan.org>
2017-12-12 07:27:51 -0500

    Push artifacts to Github on Tag
      - Update artifacts filter
      - Move publish / package to pre-deploy

--- scripts/packageRuntimes/packageRuntimes.sh
+++ scripts/packageRuntimes/packageRuntimes.sh
@@ -1,4 +1,4 @@
-cd lib && tar -zcvf   osx-x64.tar.gz   osx-x64 && cd ..
-cd lib && tar -zcvf   win-x86.tar.gz   win-x86 && cd ..
-cd lib && tar -zcvf   win-x64.tar.gz   win-x64 && cd ..
-cd lib && tar -zcvf linux-x64.tar.gz linux-x64 && cd ..
+cd lib && tar -zcvf   osx-x64.tgz   osx-x64 && cd ..
+cd lib && tar -zcvf   win-x86.tgz   win-x86 && cd ..
+cd lib && tar -zcvf   win-x64.tgz   win-x64 && cd ..
+cd lib && tar -zcvf linux-x64.tgz linux-x64 && cd ..
