f769a36f1e16451b6ed9f21412f64dc9e7a616a6
Nathan Boyd <nathan@engineernathan.org>
2017-12-13 07:50:00 -0500

    Update tsqllint.js to use self contained binary

--- tsqllint.js
+++ tsqllint.js
@@ -1,24 +1,28 @@
 #!/usr/bin/env node
+
 const { spawn } = require('child_process');
 const os = require('os');
 
 var args = process.argv.slice(2);
 
-if (os.type() === 'Linux' || os.type() === 'Darwin') {
-  args.unshift(`${__dirname}/osx-x64/TSQLLint.Console.dll`)
-  var child = spawn('dotnet', args);
-}
-else if (os.type() === 'Windows_NT') {
+if (os.type() === 'Darwin') {
+  var child = spawn(`${__dirname}/osx-x64/TSQLLint.Console`, args);
+
+} else if (os.type() === 'Linux') {
+  var child = spawn(`${__dirname}/linux-x64/TSQLLint.Console`, args);
+
+} else if (os.type() === 'Windows_NT') {
   if (os.type() === 'Windows_NT') {
     if (process.arch == 'x32') {
       var child = spawn(`${__dirname}/win-x86/TSQLLint.Console.exe`, args);
     } else if (process.arch == 'x64') {
       var child = spawn(`${__dirname}/win-x64/TSQLLint.Console.exe`, args);
+    } else{
+      throw new Error(`Invalid Platform: ${os.type()}, ${process.arch}`)
     }
-  } 
-}
-else {
-  throw new Error(`Invalid Platform: ${os.type()}`)
+  }
+} else {
+  throw new Error(`Invalid Platform: ${os.type()}, ${process.arch}`)
 }
 
 child.stdout.on('data', function(data) {
