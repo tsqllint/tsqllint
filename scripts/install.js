8e84dce54568177ed8b417866e9a6a2fd5eb3add
Nathan Boyd <nathan@engineernathan.org>
2017-12-19 07:14:56 -0500

    Add standard and update js to comply

--- scripts/install.js
+++ scripts/install.js
@@ -1,81 +1,80 @@
 #!/usr/bin/env node
 
-const https = require('follow-redirects').https;
-const os = require('os');
-const fs = require('fs');
-const decompress = require('decompress');
-const decompressTargz = require('decompress-targz');
-const ProgressBar = require('progress');
+const https = require('follow-redirects').https
+const os = require('os')
+const fs = require('fs')
+const decompress = require('decompress')
+const decompressTargz = require('decompress-targz')
+const ProgressBar = require('progress')
 
-var runTime = "";
-if (os.type() === 'Darwin') {
-    runTime = "osx-x64";
-} else if (os.type() === 'Linux'){
-    runTime = "linux-x64";
-} else if (os.type() === 'Windows_NT') {
-    if (process.arch == 'x32') {
-        runTime = 'win-x86';
-    } else if (process.arch == 'x64') {
-        runTime = "win-x64";
-    }
+var runTime = ''
+if (os.type() ==== 'Darwin') {
+  runTime = 'osx-x64'
+} else if (os.type() ==== 'Linux') {
+  runTime = 'linux-x64'
+} else if (os.type() ==== 'Windows_NT') {
+  if (process.arch === 'x32') {
+    runTime = 'win-x86'
+  } else if (process.arch === 'x64') {
+    runTime = 'win-x64'
+  }
 } else {
-    throw new Error(`Invalid Platform: ${os.type()}`)
+  throw new Error(`Invalid Platform: ${os.type()}`)
 }
 
-function download(url, dest) {
-    return new Promise((resolve, reject) => {
-        var file = fs.createWriteStream(dest);
-        var request = https.get(url, function(response) {
-            response.pipe(file);
-            file.on('finish', function() {
-                file.close(resolve);
-            });
-        })
+function download (url, dest) {
+  return new Promise((resolve, reject) => {
+    var file = fs.createWriteStream(dest)
+    var request = https.get(url, function (response) {
+      response.pipe(file)
+      file.on('finish', function () {
+        file.close(resolve)
+      })
+    })
         .on('response', (res) => {
-            if(res.statusCode != 200){
-                fs.unlink(dest);
-                return reject(new Error(`There was a problem downloading ${url}`));
-            }
+          if (res.statusCode != 200) {
+            fs.unlink(dest)
+            return reject(new Error(`There was a problem downloading ${url}`))
+          }
 
-            var len = parseInt(res.headers['content-length'], 10);
+          var len = parseInt(res.headers['content-length'], 10)
 
-            var bar = new ProgressBar(`Downloading TSQLLint ${runTime} Runtime [:bar] :rate/bps :percent :etas`, {
-                complete: '=',
-                incomplete: ' ',
-                width: 20,
-                total: len
-            });
+          var bar = new ProgressBar(`Downloading TSQLLint ${runTime} Runtime [:bar] :rate/bps :percent :etas`, {
+            complete: '=',
+            incomplete: ' ',
+            width: 20,
+            total: len
+          })
 
-            res.on('data', function (chunk) {
-                bar.tick(chunk.length);
-            });
+          res.on('data', function (chunk) {
+            bar.tick(chunk.length)
+          })
         })
-        .on('error', function(err) {
-            fs.unlink(dest);
-            reject(err);
-        });
-    });
+        .on('error', function (err) {
+          fs.unlink(dest)
+          reject(err)
+        })
+  })
 };
 
-var version = 'v1.8.4';
-var urlBase = `https://github.com/tsqllint/tsqllint/releases/download/${version}`;
+var version = 'v1.8.4'
+var urlBase = `https://github.com/tsqllint/tsqllint/releases/download/${version}`
 download(`${urlBase}/${runTime}.tgz`, `${runTime}.tgz`, (err) => {
-    if(err){
-        console.log(err);
-        return;
-    }
+  if (err) {
+    console.log(err)
+  }
 }).then((err) => {
-    if(err){
-        console.log(err);
-        return;
-    }
+  if (err) {
+    console.log(err)
+    return
+  }
 
-    decompress(`${runTime}.tgz`, './', {
-        plugins: [
-            decompressTargz()
-        ]
-    });
+  decompress(`${runTime}.tgz`, './', {
+    plugins: [
+      decompressTargz()
+    ]
+  })
 }).catch((err) => {
-    console.log(err);
-    process.exit(1);
+  console.log(err)
+  process.exit(1)
 })
--- tsqllint.js
+++ tsqllint.js
@@ -1,23 +1,21 @@
 #!/usr/bin/env node
 
-const { spawn } = require('child_process');
-const os = require('os');
+const { spawn } = require('child_process')
+const os = require('os')
 
-var args = process.argv.slice(2);
+var args = process.argv.slice(2)
 
-if (os.type() === 'Darwin') {
-  var child = spawn(`${__dirname}/osx-x64/TSQLLint.Console`, args);
-
-} else if (os.type() === 'Linux') {
-  var child = spawn(`${__dirname}/linux-x64/TSQLLint.Console`, args);
-
-} else if (os.type() === 'Windows_NT') {
-  if (os.type() === 'Windows_NT') {
-    if (process.arch == 'x32') {
-      var child = spawn(`${__dirname}/win-x86/TSQLLint.Console.exe`, args);
-    } else if (process.arch == 'x64') {
-      var child = spawn(`${__dirname}/win-x64/TSQLLint.Console.exe`, args);
-    } else{
+if (os.type() ==== 'Darwin') {
+  var child = spawn(`${__dirname}/osx-x64/TSQLLint.Console`, args)
+} else if (os.type() ==== 'Linux') {
+  var child = spawn(`${__dirname}/linux-x64/TSQLLint.Console`, args)
+} else if (os.type() ==== 'Windows_NT') {
+  if (os.type() ==== 'Windows_NT') {
+    if (process.arch === 'x32') {
+      var child = spawn(`${__dirname}/win-x86/TSQLLint.Console.exe`, args)
+    } else if (process.arch === 'x64') {
+      var child = spawn(`${__dirname}/win-x64/TSQLLint.Console.exe`, args)
+    } else {
       throw new Error(`Invalid Platform: ${os.type()}, ${process.arch}`)
     }
   }
@@ -25,10 +23,10 @@ if (os.type() === 'Darwin') {
   throw new Error(`Invalid Platform: ${os.type()}, ${process.arch}`)
 }
 
-child.stdout.on('data', function(data) {
-  process.stdout.write(data);
-});
+child.stdout.on('data', function (data) {
+  process.stdout.write(data)
+})
 
-child.stderr.on('data', function(data) {
-  process.stdout.write(data);
-});
+child.stderr.on('data', function (data) {
+  process.stdout.write(data)
+})
