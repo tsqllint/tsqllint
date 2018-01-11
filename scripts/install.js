ea3127b4ab533481ba1d8b581578d55a7c73a5f1
Nathan Boyd <nathan@engineernathan.org>
2018-01-11 17:07:47 -0500

    Release v1.8.9

--- scripts/install.js
+++ scripts/install.js
@@ -57,7 +57,7 @@ function download (url, dest) {
   })
 };
 
-var version = 'v1.8.8'
+var version = 'v1.8.9'
 var urlBase = `https://github.com/tsqllint/tsqllint/releases/download/${version}`
 download(`${urlBase}/${runTime}.tgz`, `${runTime}.tgz`, (err) => {
   if (err) {
