f88cc6dac1f8e1568b85d1093c005f2770224249
Nathan Boyd <nboyd@salesforce.com>
2018-02-02 18:57:54 -0500

    Update script path env var

--- scripts/tests/run-acceptance-tests.cmd
+++ scripts/tests/run-acceptance-tests.cmd
@@ -1,5 +1,4 @@
 git clone https://github.com/tsqllint/tsqllint-acceptance-testing.git
-SET TEST_SCRIPT_PATH="../tsqllint.js"
+SET TEST_SCRIPT_PATH=..\tsqllint.js
 cd tsqllint-acceptance-testing
-npm install
-npm run test
+npm install && npm run test
