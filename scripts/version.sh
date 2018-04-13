0a54a974daa6cadb3f08c29d83be116a69a51b0e
Nathan Boyd <nathan@engineernathan.org>
2018-04-13 14:15:59 -0400

    Update regex for package and install scripts

--- scripts/version.sh
+++ scripts/version.sh
@@ -5,11 +5,11 @@ version=$1
 csprojFiles=( "./TSQLLint.Console/TSQLLint.Console.csproj" "./TSQLLint.Lib/TSQLLint.Lib.csproj" "./TSQLLint.Tests/TSQLLint.Tests.csproj")
 for i in "${csprojFiles[@]}"
 do
-	sed -E -i .bak "s/\<Version\>([0-9]+\.){2}[0-9]\<\/Version\>/\<Version\>$version\<\/Version\>/g" $i
+	sed -E -i .bak "s/\<Version\>([0-9]+\.){2}[0-9]+\<\/Version\>/\<Version\>$version\<\/Version\>/g" $i
 done
 
-sed -E -i .bak "s/v([0-9]\.){2}[0-9]/v$version/g" ./scripts/install.js
-sed -E -i .bak "s/\"version\": \"([0-9]\.){2}[0-9]\"/\"version\": \"$version\"/g" ./package.json
+sed -E -i .bak "s/\'v([0-9]+\.){2}[0-9]+\'/'v$version'/g" ./scripts/install.js
+sed -E -i .bak "s/\"version\": \"([0-9]+\.){2}[0-9]+\"/\"version\": \"$version\"/g" ./package.json
 
 find . -name "*.js.bak" -type f -delete
 find . -name "*.json.bak" -type f -delete
