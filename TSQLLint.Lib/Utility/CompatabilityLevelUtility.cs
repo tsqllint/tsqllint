260568aff3277176eb026f709d31151b6f243f06
Nathan Boyd <nathan-boyd@users.noreply.github.com>
2018-04-19 06:14:00 -0400

    Update to allow Compatability-Level 140 (#173)

--- TSQLLint.Lib/Utility/CompatabilityLevelUtility.cs
+++ TSQLLint.Lib/Utility/CompatabilityLevelUtility.cs
@@ -6,7 +6,7 @@ namespace TSQLLint.Lib.Utility
     {
         public static int ValidateCompatabilityLevel(int compatabilityLevel)
         {
-            var validCompatibilityLevels = new List<int> { 80, 90, 100, 110, 120, 130 };
+            var validCompatibilityLevels = new List<int> { 80, 90, 100, 110, 120, 130, 140 };
             return validCompatibilityLevels.Contains(compatabilityLevel)
                 ? compatabilityLevel
                 : 120;
