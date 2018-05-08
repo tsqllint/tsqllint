a6df196c031acc20af86d4c0679e360ac8858ed8
Nathan Boyd <nboyd@salesforce.com>
2018-05-08 09:39:25 -0400

    Add condition for coverage metric reporting

--- scripts/test/cover.cmd
+++ scripts/test/cover.cmd
@@ -1,12 +1,12 @@
 nuget install -Verbosity quiet -OutputDirectory packages -Version 4.6.519 OpenCover
-nuget install -Verbosity quiet -OutputDirectory packages -Version 3.0.2 ReportGenerator
-nuget install -Verbosity quiet -OutputDirectory packages -Version 1.0.1 Codecov
+nuget install -Verbosity quiet -OutputDirectory packages -Version 3.0.2   ReportGenerator
+nuget install -Verbosity quiet -OutputDirectory packages -Version 1.0.1   Codecov
 
 SET OPENCOVER=.\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe
 SET REPORTGENERATOR=.\packages\ReportGenerator.3.0.2\tools\ReportGenerator.exe
 SET CODECOV=.\packages\Codecov.1.0.1\tools\codecov.exe
 
-RMDIR /S /Q coverage
+rmdir /Q /S coverage
 mkdir coverage
 
 %OPENCOVER% ^
@@ -21,6 +21,9 @@ mkdir coverage
     -reports:.\coverage\coverage_results.xml ^
     -targetdir:.\coverage\report\
 
-%CODECOV% ^
-    -f ".\coverage\coverage_results.xml" ^
-    -t %codecov_token%
+REM Only send coverage metrics on APPVEYOR builds
+IF NOT "%APPVEYOR%" ==  "" (
+    %CODECOV% ^
+        -f ".\coverage\coverage_results.xml" ^
+        -t %codecov_token%
+)
