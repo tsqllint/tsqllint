9dd54cfc1be8c6bc5504a6e30c2490a80c3eece1
Nathan Boyd <nboyd@salesforce.com>
2018-05-15 18:32:15 -0400

    Update schema-qualify rule to properly report dynamic sql column number

--- TSQLLint.Tests/UnitTests/LintingRules/schema-qualify/SchemaQualifyRuleTests.cs
+++ TSQLLint.Tests/UnitTests/LintingRules/schema-qualify/SchemaQualifyRuleTests.cs
@@ -8,22 +8,24 @@ namespace TSQLLint.Tests.UnitTests.LintingRules
 {
     public class SchemaQualifyRuleTests
     {
+        private const string RuleName = "schema-qualify";
+
         private static readonly object[] TestCases =
         {
             new object[]
             {
-                "schema-qualify", "schema-qualify-no-error",  typeof(SchemaQualifyRule), new List<RuleViolation>()
+                "schema-qualify-no-error", new List<RuleViolation>()
             },
             new object[]
             {
-                "schema-qualify", "schema-qualify-one-error", typeof(SchemaQualifyRule), new List<RuleViolation>
+                "schema-qualify-one-error", new List<RuleViolation>
                 {
                     new RuleViolation("schema-qualify", 1, 17)
                 }
             },
             new object[]
             {
-                "schema-qualify", "schema-qualify-two-errors", typeof(SchemaQualifyRule), new List<RuleViolation>
+                "schema-qualify-two-errors", new List<RuleViolation>
                 {
                     new RuleViolation("schema-qualify", 1, 17),
                     new RuleViolation("schema-qualify", 2, 17)
@@ -31,18 +33,44 @@ namespace TSQLLint.Tests.UnitTests.LintingRules
             },
             new object[]
             {
-                "schema-qualify", "schema-qualify-one-error-mixed-state", typeof(SchemaQualifyRule), new List<RuleViolation>
+                "schema-qualify-one-error-mixed-state", new List<RuleViolation>
                 {
                     new RuleViolation("schema-qualify", 3, 21)
                 }
             }
         };
 
-        [Test]
-        [TestCaseSource(nameof(TestCases))]
-        public void TestRule(string rule, string testFileName, Type ruleType, List<RuleViolation> expectedRuleViolations)
+        private static readonly object[] DynamicSqlTestCases =
         {
-            RulesTestHelper.RunRulesTest(rule, testFileName, ruleType, expectedRuleViolations);
+            new object[]
+            {
+                @"EXEC('SELECT FOO FROM BAR;');",
+                new List<RuleViolation>
+                {
+                    new RuleViolation(RuleName, 1, 23),
+                }
+            },
+            new object[]
+            {
+                @"EXEC('
+                    SELECT FOO FROM BAR;');",
+                new List<RuleViolation>
+                {
+                    new RuleViolation(RuleName, 2, 37),
+                }
+            }
+        };
+
+        [TestCaseSource(nameof(TestCases))]
+        public void TestRule(string testFileName, List<RuleViolation> expectedRuleViolations)
+        {
+            RulesTestHelper.RunRulesTest(RuleName, testFileName, typeof(SchemaQualifyRule), expectedRuleViolations);
+        }
+
+        [TestCaseSource(nameof(DynamicSqlTestCases))]
+        public void TestRuleWithDynamicSql(string sql, List<RuleViolation> expectedVioalations)
+        {
+            RulesTestHelper.RunDynamicSQLRulesTest(typeof(SchemaQualifyRule), sql, expectedVioalations);
         }
     }
 }
