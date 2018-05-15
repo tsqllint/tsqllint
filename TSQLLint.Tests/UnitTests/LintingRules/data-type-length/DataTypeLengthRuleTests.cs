d447ead5e9794644045a3254f053f3565402dfe2
Nathan Boyd <nboyd@salesforce.com>
2018-05-15 15:07:29 -0400

    Add tests for dynamic sql data-type rule violations

--- TSQLLint.Tests/UnitTests/LintingRules/data-type-length/DataTypeLengthRuleTests.cs
+++ TSQLLint.Tests/UnitTests/LintingRules/data-type-length/DataTypeLengthRuleTests.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using NUnit.Framework;
 using TSQLLint.Infrastructure.Rules;
@@ -8,49 +7,70 @@ namespace TSQLLint.Tests.UnitTests.LintingRules
 {
     public class DataTypeLengthRuleTests
     {
+        private const string RuleName = "data-type-length";
+
         private static readonly object[] TestCases =
         {
             new object[]
             {
-                "data-type-length", "data-type-length-no-error",  typeof(DataTypeLengthRule), new List<RuleViolation>()
+                "data-type-length-no-error", new List<RuleViolation>()
             },
             new object[]
             {
-                "data-type-length", "data-type-length-one-error", typeof(DataTypeLengthRule), new List<RuleViolation>
+                "data-type-length-one-error", new List<RuleViolation>
                 {
-                    new RuleViolation("data-type-length", 3, 19)
+                    new RuleViolation(RuleName, 3, 19)
                 }
             },
 
             new object[]
             {
-                "data-type-length", "data-type-length-one-error-mixed-state", typeof(DataTypeLengthRule), new List<RuleViolation>
+                "data-type-length-one-error-mixed-state", new List<RuleViolation>
                 {
-                    new RuleViolation("data-type-length", 7, 15)
+                    new RuleViolation(RuleName, 7, 15)
                 }
             },
             new object[]
             {
-                "data-type-length", "data-type-length-all-errors", typeof(DataTypeLengthRule), new List<RuleViolation>
+                "data-type-length-all-errors", new List<RuleViolation>
                 {
-                    new RuleViolation("data-type-length", 2,  17),
-                    new RuleViolation("data-type-length", 3,  20),
-                    new RuleViolation("data-type-length", 4,  23),
-                    new RuleViolation("data-type-length", 5,  19),
-                    new RuleViolation("data-type-length", 6,  20),
-                    new RuleViolation("data-type-length", 7,  22),
-                    new RuleViolation("data-type-length", 8,  22),
-                    new RuleViolation("data-type-length", 9,  22),
-                    new RuleViolation("data-type-length", 10, 19)
+                    new RuleViolation(RuleName, 2,  17),
+                    new RuleViolation(RuleName, 3,  20),
+                    new RuleViolation(RuleName, 4,  23),
+                    new RuleViolation(RuleName, 5,  19),
+                    new RuleViolation(RuleName, 6,  20),
+                    new RuleViolation(RuleName, 7,  22),
+                    new RuleViolation(RuleName, 8,  22),
+                    new RuleViolation(RuleName, 9,  22),
+                    new RuleViolation(RuleName, 10, 19)
                 }
             }
         };
 
-        [Test]
-        [TestCaseSource(nameof(TestCases))]
-        public void TestRule(string rule, string testFileName, Type ruleType, List<RuleViolation> expectedRuleViolations)
+        private static readonly object[] DynamicSqlTestCases =
         {
-            RulesTestHelper.RunRulesTest(rule, testFileName, ruleType, expectedRuleViolations);
+            new object[]
+            {
+                @"EXEC('CREATE TABLE MyTable 
+                    (ID INT, 
+                     Name nvarchar);');",
+                new List<RuleViolation>
+                {
+                    new RuleViolation(RuleName, 3, 35),
+                }
+            }
+        };
+
+        [TestCaseSource(nameof(TestCases))]
+        public void TestRule(string testFileName, List<RuleViolation> expectedRuleViolations)
+        {
+            RulesTestHelper.RunRulesTest(RuleName, testFileName, typeof(DataTypeLengthRule), expectedRuleViolations);
+        }
+
+        [TestCaseSource(nameof(DynamicSqlTestCases))]
+        public void TestRuleWithDynamicSql(string sql, List<RuleViolation> expectedVioalations)
+        {
+            RulesTestHelper.RunDynamicSQLRulesTest(typeof(DataTypeLengthRule), sql, expectedVioalations);
         }
     }
 }
