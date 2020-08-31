302084975712f14294e915ee172d4f358884ffc7
Nathan Boyd <nathan-boyd@users.noreply.github.com>
2020-08-31 12:05:57 -0400

    Build and publish in docker (#248)
    * build, publish, release in Travis in docker
    
    * use dotnet core 3.1 to publish assemblies

--- scripts/build/publish.sh
+++ scripts/build/publish.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
-set -e
-
-WORKING_DIRECTORY=$(pwd)
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BUILDSCRIPTDIR="$(dirname "$SCRIPTDIR")"
 REPODIR="$(dirname "$BUILDSCRIPTDIR")"
-ASSEMBLIESDIR=$REPODIR/artifacts/assemblies
-mkdir -p $ASSEMBLIESDIR
+cd "$REPODIR"
 
-dotnet publish $REPODIR/source/TSQLLint.Console/TSQLLint.Console.csproj -c Release -f netcoreapp3.1 -r win-x86   -o $ASSEMBLIESDIR/win-x86
-dotnet publish $REPODIR/source/TSQLLint.Console/TSQLLint.Console.csproj -c Release -f netcoreapp3.1 -r win-x64   -o $ASSEMBLIESDIR/win-x64
-dotnet publish $REPODIR/source/TSQLLint.Console/TSQLLint.Console.csproj -c Release -f netcoreapp3.1 -r osx-x64   -o $ASSEMBLIESDIR/osx-x64
-dotnet publish $REPODIR/source/TSQLLint.Console/TSQLLint.Console.csproj -c Release -f netcoreapp3.1 -r linux-x64 -o $ASSEMBLIESDIR/linux-x64
+set -e
 
-tar -zcvf $ASSEMBLIESDIR/osx-x64.tgz   $ASSEMBLIESDIR/osx-x64
-tar -zcvf $ASSEMBLIESDIR/win-x86.tgz   $ASSEMBLIESDIR/win-x86
-tar -zcvf $ASSEMBLIESDIR/win-x64.tgz   $ASSEMBLIESDIR/win-x64
-tar -zcvf $ASSEMBLIESDIR/linux-x64.tgz $ASSEMBLIESDIR/linux-x64
+dotnet publish ./source/TSQLLint.Console/TSQLLint.Console.csproj -c Release -f netcoreapp3.1 -r win-x86   -o ./assemblies/win-x86
+dotnet publish ./source/TSQLLint.Console/TSQLLint.Console.csproj -c Release -f netcoreapp3.1 -r win-x64   -o ./assemblies/win-x64
+dotnet publish ./source/TSQLLint.Console/TSQLLint.Console.csproj -c Release -f netcoreapp3.1 -r osx-x64   -o ./assemblies/osx-x64
+dotnet publish ./source/TSQLLint.Console/TSQLLint.Console.csproj -c Release -f netcoreapp3.1 -r linux-x64 -o ./assemblies/linux-x64
