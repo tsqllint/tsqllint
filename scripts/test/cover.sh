089806611f31a5d6595aa99614c13c4c3ab3ad6c
Nathan Boyd <nboyd@salesforce.com>
2020-08-31 14:51:33 -0400

    comnine cover and report build stages

--- scripts/test/cover.sh
+++ scripts/test/cover.sh
@@ -5,11 +5,20 @@ SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BUILDSCRIPTDIR="$(dirname "$SCRIPTDIR")"
 REPODIR="$(dirname "$BUILDSCRIPTDIR")"
 COVERAGEDIR="$REPODIR/artifacts/coverage"
+COVERAGEFILE="$(find $COVERAGEDIR -name coverage.opencover.xml)"
 
-mkdir -p $COVERAGEDIR
+mkdir -p "$COVERAGEDIR/report"
 
 dotnet test \
     --collect:"XPlat Code Coverage" \
     --settings $REPODIR/source/coverlet.runsettings \
     --results-directory $COVERAGEDIR \
     $REPODIR/source/TSQLLint.sln
+
+command -v reportgenerator >/dev/null 2>&1 || { dotnet tool install --global dotnet-reportgenerator-globaltool --version 4.6.5; }
+
+reportgenerator \
+    -reports:$COVERAGEFILE \
+    -targetdir:$COVERAGEDIR/report
+
+tar -zcvf $COVERAGEDIR/coverage-report.tgz   $COVERAGEDIR/report
