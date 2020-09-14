f3fb5161127afffed64d506a3155b237bfd53a91
Nathan Boyd <nboyd@salesforce.com>
2020-09-14 08:26:03 -0400

    push to coveralls

--- scripts/test/cover.sh
+++ scripts/test/cover.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+PATH="$PATH:/root/.dotnet/tools"
+
 WORKING_DIRECTORY=$(pwd)
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BUILDSCRIPTDIR="$(dirname "$SCRIPTDIR")"
@@ -17,7 +19,7 @@ COVERAGEFILE="$(find $COVERAGEDIR -name coverage.opencover.xml)"
 # if necessary install reportgenerator
 command -v reportgenerator >/dev/null 2>&1 || { dotnet tool install --global dotnet-reportgenerator-globaltool --version 4.6.5; }
 
-/root/.dotnet/tools/reportgenerator \
+reportgenerator \
     -reports:$COVERAGEFILE \
     -targetdir:$COVERAGEDIR/report
 
@@ -25,3 +27,9 @@ command -v reportgenerator >/dev/null 2>&1 || { dotnet tool install --global dot
 cd $COVERAGEDIR
 tar -zcvf $COVERAGEDIR/coverage-report.tgz report
 cd $WORKING_DIRECTORY
+
+if [[ ! -z "${TRAVIS}" ]]; then
+  command -v csmacnz.Coveralls >/dev/null 2>&1 || { dotnet tool install --global coveralls.net --version 2.0.0-beta.1; }
+  # send coverage report to coveralls
+  csmacnz.Coveralls --opencover -i "$COVERAGEFILE"
+fi
