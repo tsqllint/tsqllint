babd6251e519a4e3be660d4eef82a69a76d2f283
Stephen Weaver <stephen.weaver@salesforce.com>
2022-04-30 15:05:42 -0600

    Add FindViolatingNode to BaseRuleVisitor

--- source/TSQLLint.Infrastructure/Rules/Common/BaseRuleVisitor.cs
+++ source/TSQLLint.Infrastructure/Rules/Common/BaseRuleVisitor.cs
@@ -1,5 +1,7 @@
 using Microsoft.SqlServer.TransactSql.ScriptDom;
 using System;
+using System.IO;
+using System.Linq;
 using TSQLLint.Common;
 using TSQLLint.Core.Interfaces;
 
@@ -19,8 +21,49 @@ namespace TSQLLint.Infrastructure.Rules.Common
         public abstract string RULE_NAME { get; }
         public abstract string RULE_TEXT { get; }
 
+        public virtual T FindViolatingNode<T>(string[] fileLines, IRuleViolation ruleViolation)
+            where T : TSqlFragment
+        {
+            using var rdr = new StringReader(string.Join("\n", fileLines));
+            var parser = new TSql150Parser(true, SqlEngineType.All);
+            var tree = parser.Parse(rdr, out var errors);
+            if (errors?.Any() == true)
+            {
+                throw new Exception($"Parsing failed. {string.Join(". ", errors.Select(x => x.Message))}");
+            }
+            var checker = new FineViolatingNodeVisitor<T>(ruleViolation);
+            tree.Accept(checker);
+            return checker.Node;
+        }
+
         public virtual void FixViolation(string[] fileLines, IRuleViolation ruleViolation)
         {
+        }
+
+        internal class FineViolatingNodeVisitor<T> : TSqlFragmentVisitor
+            where T : TSqlFragment
+        {
+            public T Node;
+            private readonly IRuleViolation ruleViolation;
+
+            public FineViolatingNodeVisitor(IRuleViolation ruleViolation)
+            {
+                this.ruleViolation = ruleViolation;
+            }
+
+            public override void Visit(TSqlFragment node)
+            {
+                if (node is T fragment &&
+                    fragment.StartLine == ruleViolation.Line &&
+                    fragment.StartColumn == ruleViolation.Column)
+                {
+                    Node = fragment;
+
+                    return;
+                }
+
+                base.Visit(node);
+            }
         }
     }
 }
