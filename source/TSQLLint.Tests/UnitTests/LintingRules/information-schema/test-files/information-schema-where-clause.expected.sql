81c4183733378f1e588c47b8558842beb63851b1
Stephen Weaver <stephen.weaver@salesforce.com>
2022-05-04 13:46:37 -0600

    Update expected

--- source/TSQLLint.Tests/UnitTests/LintingRules/information-schema/test-files/information-schema-where-clause.expected.sql
+++ source/TSQLLint.Tests/UnitTests/LintingRules/information-schema/test-files/information-schema-where-clause.expected.sql
@@ -13,9 +13,7 @@ END
 
 
 IF EXISTS (SELECT 1 FROM sys.columns 
-	WHERE [object_id] = OBJECT_ID(N'dbo.tableName')
-		AND [name] = 'columnName' 
-		AND [system_type_id] = TYPE_ID(N'nvarchar'))
+	WHERE [object_id] = OBJECT_ID(N'dbo.tableName') AND [name] = 'columnName' AND [system_type_id] = TYPE_ID(N'nvarchar'))
 BEGIN
 SELECT 1;
 END
